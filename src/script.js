54ba253380fec0d6a613dfb7cf42e89364ffe337
navidvfie <info@navidvafaei.ir>
2024-08-27 15:07:38 +0330

    amazing order slide

--- src/script.js
+++ src/script.js
@@ -52,11 +52,43 @@ const swiper = new Swiper('.story-box', {
   });
 
    const swiperAmazingOrder = new Swiper('.amazing-order', {
-    slidesPerView : 10.5,  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
+    slidesPerView : 6.5,
+    freeMode: true ,
+
+  // Optional parameters
+  direction: 'horizontal',
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+
   });
+
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 5, 2030 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="countdown-timer"
+  document.querySelector(".countdown-timer-h").innerHTML = hours;
+  document.querySelector(".countdown-timer-m").innerHTML = minutes;
+  document.querySelector(".countdown-timer-s").innerHTML = seconds;
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.querySelector(".countdown-timer").innerHTML = "EXPIRED";
+  }
+}, 1000);
